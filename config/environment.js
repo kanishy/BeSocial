1feb9baf9a29ca2cd38fe2794a523cbb95ccfe6c
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2026-07-06 08:18:04 +0530

    pushing the remaing code in local

---
+++ config/environment.js
@@ -0,0 +1,14 @@
+const development = {
+    name: 'development',
+    asset_path: '/assets',
+    session_cookie_key: "somethingrandom",
+    db: 'besocial_development',
+    smtp: 'transporter object',
+}
+
+const production = {
+    name: 'production',
+}
+
+
+module.exports = development;
